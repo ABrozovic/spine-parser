e519892d2ee3c91f6865245e0fafc823257e5965
Z <z>
2023-05-22 16:56:18 -0400

    feat: add ui components

---
+++ components/combobox.tsx
@@ -0,0 +1,101 @@
+import * as React from "react"
+import { Check, ChevronsUpDown } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
+type ComboBoxProps = {
+  data: { value: string; label: string }[] | undefined
+  onChange: (value: string) => void
+  className?: string
+  defaultToFirst?: boolean
+  selectText?: string
+  searchText?: string
+  disabledText?: string
+  notFoundText?: string
+}
+
+const buttonRef = React.createRef<HTMLButtonElement>()
+
+export function ComboBox({
+  data,
+  searchText,
+  selectText,
+  onChange,
+  disabledText,
+  notFoundText,
+}: ComboBoxProps) {
+  const [open, setOpen] = React.useState(false)
+  const [value, setValue] = React.useState("")
+  const [popoverWidth, setPopoverWidth] = React.useState<string>("200px")
+
+  React.useLayoutEffect(() => {
+    if (open) {
+      const buttonWidth = buttonRef.current?.offsetWidth
+      setPopoverWidth(`${buttonWidth}px`)
+    }
+  }, [open])
+
+  const onSelect = (currentValue: string, id: string): void => {
+    setValue(currentValue)
+    onChange(id)
+    setOpen(false)
+  }
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          ref={buttonRef}
+          disabled={!data}
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-full justify-between capitalize"
+        >
+          {!data
+            ? disabledText
+            : value
+            ? data.find((option) => option.label === value)?.label
+            : selectText ?? "Select an option..."}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50 " />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto" style={{ width: popoverWidth }}>
+        <Command>
+          <CommandInput placeholder={searchText ?? "Search an option..."} />
+          <CommandEmpty>{notFoundText ?? "No option found."}</CommandEmpty>
+          <CommandGroup>
+            {data?.map((option) => (
+              <CommandItem
+                className="capitalize"
+                key={option.value}
+                onSelect={(label) => onSelect(label, option.value)}
+              >
+                <Check
+                  className={cn(
+                    "mr-2 h-4 w-4",
+                    value === option.value ? "opacity-100" : "opacity-0"
+                  )}
+                />
+                {option.label}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  )
+}
---
+++ components/dropzone.tsx
@@ -0,0 +1,84 @@
+import { useCallback, useState } from "react"
+import { buildAcceptedObjects, type CommonMimeType } from "@/utils/mime-types"
+import { useDropzone } from "react-dropzone"
+
+import useFileInput, { type FileWithId } from "@/hooks/use-file-input"
+
+type DropzoneProps = {
+  onFilesChanged: (files: FileWithId[]) => void
+  onError?: (err: string) => void
+  multiple?: boolean
+  acceptedMimeTypes?: CommonMimeType[]
+  acceptAll?: boolean
+  defaultValue?: FileWithId[]
+  maxFiles?: number
+  className?: string
+
+  validator?: <T extends FileWithId[]>(file: T) => FileWithId[]
+  children: ({
+    files,
+    isDragAccept,
+    maxFilesReached,
+    handleRemove,
+    reset,
+    error,
+  }: {
+    files: FileWithId[]
+    isDragAccept: boolean
+    maxFilesReached: boolean
+    handleRemove: (index: number) => void
+    reset: () => void
+    error?: string
+  }) => JSX.Element
+}
+const Dropzone = ({
+  onFilesChanged,
+  acceptedMimeTypes = ["image/png", "image/jpeg"],
+  acceptAll,
+  defaultValue = [],
+  multiple = true,
+  maxFiles,
+  className,
+  validator,
+  children,
+}: DropzoneProps) => {
+  const [error, setError] = useState<string>()
+
+  const { files, handleDrop, reset, handleRemove } = useFileInput({
+    defaultFiles: defaultValue,
+    validator,
+    onError: setError,
+    onFilesChanged,
+  })
+
+  const { getRootProps, getInputProps, isDragAccept } = useDropzone({
+    maxFiles,
+    multiple,
+    accept: acceptAll ? undefined : buildAcceptedObjects(acceptedMimeTypes),
+    onDrop: handleDrop,
+  })
+
+  const maxFilesReached = useCallback(
+    () => !!maxFiles && files.length >= maxFiles,
+    [files.length, maxFiles]
+  )
+
+  return (
+    <div className={className}>
+      <div {...getRootProps()}>
+        {children({
+          files,
+          isDragAccept,
+          maxFilesReached: maxFilesReached(),
+          reset,
+          handleRemove,
+          error,
+        })}
+        <input {...getInputProps()} className="hidden" />
+      </div>
+      {error && <div className="mt-1 text-center text-red-500">{error} </div>}
+    </div>
+  )
+}
+
+export default Dropzone
