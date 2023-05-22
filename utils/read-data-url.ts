f348810e9e412b6940f0636a611b5799712ecc44
Z <z>
2023-05-22 16:58:11 -0400

    feat: add file reader as url util

---
+++ utils/read-data-url.ts
@@ -0,0 +1,8 @@
+export const readFileAsDataURL = (file: Blob): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.onload = () => resolve(reader.result as string)
+    reader.onerror = (error) => reject(error)
+    reader.readAsDataURL(file)
+  })
+}
